82d116bee0128183e863e14fb5373c5d7d2076e9
Eduardo Muchak <eduardomuchak@gmail.com>
2023-03-27 15:22:34 -0300

    feat: add and configure sanofi custom fonts and color palette

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => (
-  <div className="bg-red-400 w-full h-full">
-    <h1>teste</h1>
+  <div className="bg-red-400 w-screen h-screen flex items-center justify-center">
+    <h1 className="font-sanofiSansRegular text-9xl">Teste</h1>
   </div>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sanofiSansBold: ['sanofiSansBold', 'sans-serif'],
+        sanofiSansBoldItalic: ['sanofiSansBoldItalic', 'sans-serif'],
+        sanofiSansItalic: ['sanofiSansItalic', 'sans-serif'],
+        sanofiSansRegular: ['sanofiSansRegular', 'sans-serif'],
+        sanofiSansLight: ['sanofiSansLight', 'sans-serif'],
+        sanofiSansLightItalic: ['sanofiSansLightItalic', 'sans-serif'],
+      },
+      colors: {
+        sanofiPurpleDark: '#22004C',
+        sanofiPurple: '#7A01E6',
+        sanofiGray: '#F4F2F7',
+        secondaryBlue: '#525CA3',
+        secondaryRed: '#CF483E',
+        secondaryPink: '#BE006B',
+        secondaryGreen: '#478040',
+      },
+    },
   },
   plugins: [],
 };

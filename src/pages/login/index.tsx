5d6585a46b3e95e278077a58c2dd9193e55bd683
G-PPeralta <prates_peralta@hotmail.com>
2023-03-30 14:21:04 -0300

    feat: implements auth store and logic

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,11 @@
-import React, { ChangeEvent, useState } from 'react';
-import Image from 'next/image';
-import { Input } from 'presentation/ui/Input';
-import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
+import React, { ChangeEvent, useState } from "react";
+import Image from "next/image";
+import { Input } from "presentation/ui/Input";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 
-import logo from 'assets/images/sanofi-logo.svg';
-import { Button } from 'presentation/ui/Button';
+import logo from "assets/images/sanofi-logo.svg";
+import { Button } from "presentation/ui/Button";
+import { useAuthStore } from "store/auth";
 
 const EyeIcon = ({
   isPasswordVisible,
@@ -28,9 +29,12 @@ const EyeIcon = ({
 };
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const signin = useAuthStore((state) => state.signin);
+  const user = useAuthStore((state) => state.user);
 
   const handleEmail = (event: ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
@@ -40,12 +44,21 @@ export default function Login() {
     setPassword(event.target.value);
   };
 
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    await signin(email, password);
+    console.log(user);
+  }
+
   return (
     <div className="h-screen w-screen bg-sanofiGray flex items-center justify-center">
-      <form className="w-full max-w-[480px] flex flex-col  px-10 py-12 bg-white rounded-[32px]">
+      <form
+        className="w-full max-w-[480px] flex flex-col  px-10 py-12 bg-white rounded-[32px]"
+        onSubmit={handleSubmit}
+      >
         <Image
           src={logo}
-          alt={'Sanofi Logo'}
+          alt={"Sanofi Logo"}
           className="w-full max-w-[300px] mb-10 mx-auto"
         />
         <div className="flex flex-col gap-6 w-full">
@@ -65,7 +78,7 @@ export default function Login() {
             // }
             label="Password"
             id="password"
-            type={isPasswordVisible ? 'text' : 'password'}
+            type={isPasswordVisible ? "text" : "password"}
             value={password}
             onChange={(event) => handlePassword(event)}
           />
@@ -73,7 +86,9 @@ export default function Login() {
         <span className="flex self-end my-4 font-sanofiSansRegular text-sm">
           Esqueceu a senha?
         </span>
-        <Button variant="primary">Entrar</Button>
+        <Button variant="primary" type="submit">
+          Entrar
+        </Button>
       </form>
     </div>
   );

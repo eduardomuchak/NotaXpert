de7c12a7e8d208f94ff5eecd374effd3eb27fcc4
Yolanda Ferreira de Souza <landacontato@gmail.com>
2023-03-30 15:43:01 -0300

    fix: organize files FilterTable

---
+++ src/presentation/ui/FilterTable/Types.tsx
@@ -0,0 +1,12 @@
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import { CheckboxDemo } from "./Checkbox";
+
+export function Types() {
+    return (
+      <DropdownMenu.Content className='w-24 border-black border-solid border-2 bg-white p-2 flex flex-col gap-2' sideOffset={5}>
+        <CheckboxDemo props={{label:"NFs"}} />
+        <CheckboxDemo props={{label:"DANFs"}} />
+        <DropdownMenu.Arrow className="DropdownMenuArrow" />
+      </DropdownMenu.Content>
+    );
+  }

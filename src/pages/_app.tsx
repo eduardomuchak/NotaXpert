ac48a6152075f891c36300af7a413935a3fbf155
G-PPeralta <prates_peralta@hotmail.com>
2023-03-30 17:26:54 -0300

    fix: implements logic to remove header from certain pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
-import 'styles/globals.css';
-import type { AppProps } from 'next/app';
-import { Header } from 'presentation/ui/Header';
+import type { AppProps } from "next/app";
+import { Header } from "presentation/ui/Header";
+
+import { useRouter } from "next/router";
+
+import "styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const headlessRoutes = ["/login", "/recuperar-senha", "/redefinir-senha"];
+  const { pathname } = useRouter();
+
   return (
     <>
-      <Header />
+      {headlessRoutes.includes(pathname) ? null : <Header />}
       <Component {...pageProps} />
     </>
   );
 }
+

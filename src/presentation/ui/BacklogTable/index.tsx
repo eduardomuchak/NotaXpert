9da4e44607a4fb9365c8bb8a3323e2fb2b761cb8
Eduardo Muchak <eduardomuchak@gmail.com>
2023-03-31 16:31:53 -0300

    style: :art: adjusts overflow x of table component

--- src/presentation/ui/BacklogTable/index.tsx
+++ src/presentation/ui/BacklogTable/index.tsx
@@ -1,10 +1,10 @@
-import { TableBody } from "./TableBody";
-import { TableHead } from "./TableHead";
-import FilterTable from "presentation/ui/FilterTable";
+import { TableBody } from './TableBody';
+import { TableHead } from './TableHead';
+import FilterTable from 'presentation/ui/FilterTable';
 
-import { tableHeadItems } from "./mocks";
-import { nfs } from "./mocks";
-import { ButtonView } from "../ButtonView";
+import { tableHeadItems } from './mocks';
+import { nfs } from './mocks';
+import { ButtonView } from '../ButtonView';
 import { KanbanView } from 'presentation/ui/KanbanView';
 import create from 'zustand';
 // import { NotificationCard } from "../NotificationCard";
@@ -16,31 +16,29 @@ type MyStoreType = {
 
 export const useMyStore = create<MyStoreType>((set) => ({
   isButtonPressed: false,
-  toggleButton: () => set((state) => ({ isButtonPressed: !state.isButtonPressed })),
+  toggleButton: () =>
+    set((state) => ({ isButtonPressed: !state.isButtonPressed })),
 }));
 
 export function BacklogTable() {
   const { isButtonPressed, toggleButton } = useMyStore();
   return (
     <>
-      <div className="w-full overflow-x-scroll">
-        <div className='flex flex-wrap-reverse justify-between w-full}'>
+      <div className="w-full overflow-x-auto">
+        <div className="flex flex-wrap-reverse justify-between w-full}">
           <div>
             <FilterTable />
           </div>
           <ButtonView isBoard={true} />
         </div>
-        {isButtonPressed == false ?
-          (
-            <table className="w-full mb-2 bg-gray-100">
-              <TableHead headContent={tableHeadItems} />
-              <TableBody backlog={nfs} />
-            </table>
-          )
-          :
-          (
-            <KanbanView kanbanData={{backlog:nfs}} />
-          )}
+        {isButtonPressed == false ? (
+          <table className="w-full mb-2 bg-gray-100">
+            <TableHead headContent={tableHeadItems} />
+            <TableBody backlog={nfs} />
+          </table>
+        ) : (
+          <KanbanView kanbanData={{ backlog: nfs }} />
+        )}
       </div>
     </>
   );

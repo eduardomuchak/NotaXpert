f59d3806a8d565dd1f40480e390a28cfb28b5018
G-PPeralta <prates_peralta@hotmail.com>
2023-04-03 16:48:29 -0300

    feat: creates login error modal

--- src/presentation/components/Button/index.tsx
+++ src/presentation/components/Button/index.tsx
@@ -1,5 +1,5 @@
-import clsx from 'clsx';
-import { CustomButton } from './interface';
+import clsx from "clsx";
+import { CustomButton } from "./interface";
 
 export function Button({
   children,
@@ -9,17 +9,17 @@ export function Button({
   return (
     <button
       className={clsx(
-        'h-12 rounded-full font-sanofiSansRegular text-xl transition-all  disabled:hover:cursor-not-allowed',
+        "h-12 rounded-full font-sanofiSansRegular text-xl transition-all  disabled:hover:cursor-not-allowed",
         {
-          'bg-sanofiPurpleDark text-white hover:bg-sanofiPurple disabled:opacity-80 disabled:hover:bg-sanofiPurpleDark':
-            variant === 'primary',
-          'text-sanofiPurpleDark border-2 border-sanofiPurpleDark hover:bg-sanofiPurpleDark hover:text-white disabled:bg-sanofiGray disabled:text-sanofiPurpleDark ':
-            variant === 'secondary',
-        },
+          "bg-sanofiPurpleDark text-white hover:bg-sanofiPurple disabled:opacity-80 disabled:hover:bg-sanofiPurpleDark":
+            variant === "primary",
+          "text-sanofiPurpleDark border-2 border-sanofiPurpleDark hover:bg-sanofiPurpleDark hover:text-white disabled:bg-sanofiGray disabled:text-sanofiPurpleDark ":
+            variant === "secondary",
+        }
       )}
       {...rest}
     >
-      <span>{children}</span>
+      <span className="px-4">{children}</span>
     </button>
   );
 }
--- src/presentation/components/ui/LoginUi/index.tsx
+++ src/presentation/components/ui/LoginUi/index.tsx
@@ -1,21 +1,23 @@
-import { ChangeEvent, useState } from 'react';
-import Image from 'next/image';
-import { Input } from 'presentation/components/Input';
-import logo from 'assets/images/sanofi-logo.svg';
-import { Button } from 'presentation/components/Button';
-import { ToggleEyePassword } from 'presentation/components/ToggleEyePassword';
-import Link from 'next/link';
-import { AuthUser } from 'domain/usecases/AuthUser';
-import { useAuthStore } from 'presentation/hooks/auth';
+import { ChangeEvent, useState } from "react";
+import Image from "next/image";
+import { Input } from "presentation/components/Input";
+import logo from "assets/images/sanofi-logo.svg";
+import { Button } from "presentation/components/Button";
+import { ToggleEyePassword } from "presentation/components/ToggleEyePassword";
+import Link from "next/link";
+import { AuthUser } from "domain/usecases/AuthUser";
+import { useAuthStore } from "presentation/hooks/auth";
+import { LoginErrorModal } from "../LoginErrorModal";
 
 interface LoginUIProps {
   authUser: AuthUser;
 }
 
 export function LoginUI(props: LoginUIProps) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isErrorModalVisible, setIsErrorModalVisible] = useState(true);
 
   const signin = useAuthStore((state) => state.signin);
 
@@ -42,54 +44,63 @@ export function LoginUI(props: LoginUIProps) {
     signin({ email, password });
   }
 
+  function onCloseErrorModal() {
+    setIsErrorModalVisible(false);
+  }
+
   return (
-    <div className="h-screen w-screen bg-sanofiGray flex items-center justify-center">
-      <form
-        className="w-full max-w-[540px] flex flex-col px-12 py-14 bg-white rounded-[32px]"
-        onSubmit={handleSubmit}
-        autoComplete="off"
-      >
-        <Image
-          src={logo}
-          alt={'Sanofi Logo'}
-          className="w-full max-w-[300px] mb-10 mx-auto"
-        />
-        <div className="flex flex-col gap-6 w-full">
-          <Input
-            label="Email"
-            id="email"
-            type="email"
-            value={email}
-            onChange={(event) => handleEmail(event)}
-          />
-          <Input
-            icon={
-              <ToggleEyePassword
-                isPasswordVisible={isPasswordVisible}
-                setIsPasswordVisible={setIsPasswordVisible}
-                onClick={(event) => {
-                  event.preventDefault();
-                  setIsPasswordVisible(!isPasswordVisible);
-                }}
-              />
-            }
-            label="Password"
-            id="password"
-            type={isPasswordVisible ? 'text' : 'password'}
-            value={password}
-            onChange={(event) => handlePassword(event)}
-          />
-        </div>
-        <Link
-          href="/recuperar-senha"
-          className="flex self-end my-4 font-sanofiSansRegular text-sm hover:underline"
+    <>
+      {isErrorModalVisible ? (
+        <LoginErrorModal onClose={onCloseErrorModal} />
+      ) : null}
+      <div className="h-screen w-screen bg-sanofiGray flex items-center justify-center">
+        <form
+          className="w-full max-w-[540px] flex flex-col px-12 py-14 bg-white rounded-[32px]"
+          onSubmit={handleSubmit}
+          autoComplete="off"
         >
-          Esqueceu a senha?
-        </Link>
-        <Button variant="primary" type="submit">
-          Entrar
-        </Button>
-      </form>
-    </div>
+          <Image
+            src={logo}
+            alt={"Sanofi Logo"}
+            className="w-full max-w-[300px] mb-10 mx-auto"
+          />
+          <div className="flex flex-col gap-6 w-full">
+            <Input
+              label="Email"
+              id="email"
+              type="email"
+              value={email}
+              onChange={(event) => handleEmail(event)}
+            />
+            <Input
+              icon={
+                <ToggleEyePassword
+                  isPasswordVisible={isPasswordVisible}
+                  setIsPasswordVisible={setIsPasswordVisible}
+                  onClick={(event) => {
+                    event.preventDefault();
+                    setIsPasswordVisible(!isPasswordVisible);
+                  }}
+                />
+              }
+              label="Password"
+              id="password"
+              type={isPasswordVisible ? "text" : "password"}
+              value={password}
+              onChange={(event) => handlePassword(event)}
+            />
+          </div>
+          <Link
+            href="/recuperar-senha"
+            className="flex self-end my-4 font-sanofiSansRegular text-sm hover:underline"
+          >
+            Esqueceu a senha?
+          </Link>
+          <Button variant="primary" type="submit">
+            Entrar
+          </Button>
+        </form>
+      </div>
+    </>
   );
 }

d1b35a3689532b30b71aa1bc1c6f30a59194577e
G-PPeralta <prates_peralta@hotmail.com>
2023-04-04 14:50:11 -0300

    feat: implements barChart logic

--- src/pages/estatisticas/index.tsx
+++ src/pages/estatisticas/index.tsx
@@ -1,18 +1,7 @@
-import { BarChart } from "presentation/components/BarChart";
-import { PageContainer } from "presentation/components/ui/PageUi/PageContainer";
-import { StatisticsUi } from "presentation/components/ui/StatisticsUi";
-import { withAuth } from "security/withAuth";
+import { StatisticsUi } from 'presentation/components/ui/StatisticsUi';
+import { withAuth } from 'security/withAuth';
 
 function Estatisticas() {
-  return (
-    <div>
-      <PageContainer>
-        <StatisticsUi />
-        <div className="w-1/2">
-          <BarChart />
-        </div>
-      </PageContainer>
-    </div>
-  );
+  return <StatisticsUi />;
 }
 export default withAuth(Estatisticas);

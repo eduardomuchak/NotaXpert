59d12bf2afc93978c012a68f654e05b1e85553dc
Eduardo Muchak <eduardomuchak@gmail.com>
2023-04-10 15:53:52 -0300

    feat: :sparkles: add tab pagination system on service invoice modal

--- src/presentation/components/ServiceInvoiceModal/index.tsx
+++ src/presentation/components/ServiceInvoiceModal/index.tsx
@@ -1,15 +1,24 @@
 import * as Dialog from '@radix-ui/react-dialog';
+import * as Tabs from '@radix-ui/react-tabs';
+import clsx from 'clsx';
+import { useState } from 'react';
 
 import { focusStyles } from 'styles/focus';
 import { overlayStyle } from 'styles/overlay';
 
-import { Button } from '../Button';
-import { PriorityTag } from '../PriorityTag';
-
-import { LineInfo } from './LineInfo';
-import { StatusTag } from './StatusTag';
+import { StatusTab } from './tabs/StatusTab';
 
 export function ServiceInvoiceModal() {
+  const [activeTab, setActiveTab] = useState('status');
+
+  const triggerStyle = (tab: string) =>
+    clsx('rounded-full transition-all disabled:hover:cursor-not-allowed', {
+      'bg-sanofiPurpleDark border-2 border-sanofiPurpleDark text-white hover:bg-sanofiPurple hover:border-sanofiPurple disabled:opacity-80 disabled:hover:bg-sanofiPurpleDark':
+        activeTab === tab,
+      'text-sanofiPurpleDark border-2 border-sanofiPurpleDark hover:bg-sanofiPurpleDark hover:text-white disabled:bg-sanofiGray disabled:text-sanofiPurpleDark':
+        activeTab !== tab,
+    });
+
   return (
     <Dialog.Root>
       <Dialog.Trigger
@@ -19,9 +28,9 @@ export function ServiceInvoiceModal() {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className={`${overlayStyle}`} />
-        <Dialog.Content className="fixed inset-0 m-auto h-fit flex bg-white rounded-[64px] shadow-lg p-8 w-full max-w-[1280px]">
+        <Dialog.Content className="fixed inset-0 m-auto h-fit flex bg-white rounded-[64px] shadow-lg p-8 w-full max-w-[1280px] flex-col gap-8 md:flex-row md:gap-0">
           {/* Container esquerdo */}
-          <div className="flex flex-col grow-[3] gap-4">
+          <div className="flex flex-col grow-[2] gap-4">
             <div className="flex flex-col">
               <Dialog.Title className="font-sanofiSansBold text-2xl leading-8">
                 Nota Fiscal de Serviço
@@ -30,41 +39,33 @@ export function ServiceInvoiceModal() {
                 5051354
               </span>
             </div>
-            <div className="flex gap-2 mb-4">
-              <Button variant="small-primary">Status</Button>
-              <Button variant="small-secondary">Histórico</Button>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 grid-flow-row gap-8 pr-8">
-              <LineInfo title={'Agente:'}>
-                <span className="font-sanofiSansRegular text-2xl leading-8">
-                  Roberto Miranda
-                </span>
-              </LineInfo>
-              <LineInfo title={'Status atual:'}>
-                <StatusTag status={'Em tratamento'} />
-              </LineInfo>
-              <LineInfo title={'Adicionado em:'}>
-                <span className="font-sanofiSansRegular text-2xl leading-8">
-                  22/02/2023
-                </span>
-              </LineInfo>
-              <LineInfo title={'Vence em:'}>
-                <span className="font-sanofiSansRegular text-2xl leading-8">
-                  25/02/2023
-                </span>
-              </LineInfo>
-              <LineInfo title={'Dias no board:'}>
-                <span className="font-sanofiSansRegular text-2xl leading-8">
-                  03
-                </span>
-              </LineInfo>
-              <LineInfo title={'Prioridade:'}>
-                <PriorityTag priority={'Urgente'} variant={'primary'} />
-              </LineInfo>
-            </div>
+            <Tabs.Root
+              onValueChange={(value: string) => setActiveTab(value)}
+              defaultValue={'status'}
+            >
+              <Tabs.List>
+                <Tabs.Trigger className={triggerStyle('status')} value="status">
+                  <span className="px-3 py-1 font-sanofiSansRegular text-base leading-5 text-center flex items-center justify-center ">
+                    Status
+                  </span>
+                </Tabs.Trigger>
+                <Tabs.Trigger
+                  className={triggerStyle('historic')}
+                  value="historic"
+                >
+                  <span className="px-3 py-1 font-sanofiSansRegular text-base leading-5 text-center flex items-center justify-center ">
+                    Histórico
+                  </span>
+                </Tabs.Trigger>
+              </Tabs.List>
+              <Tabs.Content value="status" defaultValue="status">
+                <StatusTab />
+              </Tabs.Content>
+              <Tabs.Content value="historic"></Tabs.Content>
+            </Tabs.Root>
           </div>
           {/* Container direito */}
-          <div className="flex flex-col grow-[2] bg-emerald-500">
+          <div className="flex flex-col grow-[3] bg-emerald-500">
             <Dialog.Description>Dados</Dialog.Description>
           </div>
         </Dialog.Content>
---
+++ src/presentation/components/ServiceInvoiceModal/tabs/StatusTab.tsx
@@ -0,0 +1,35 @@
+import { PriorityTag } from 'presentation/components/PriorityTag';
+
+import { LineInfo } from '../LineInfo';
+import { StatusTag } from '../StatusTag';
+
+export function StatusTab() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 grid-flow-row gap-8 pr-8">
+      <LineInfo title={'Agente:'}>
+        <span className="font-sanofiSansRegular text-2xl leading-8">
+          Roberto Miranda
+        </span>
+      </LineInfo>
+      <LineInfo title={'Status atual:'}>
+        <StatusTag status={'Em tratamento'} />
+      </LineInfo>
+      <LineInfo title={'Adicionado em:'}>
+        <span className="font-sanofiSansRegular text-2xl leading-8">
+          22/02/2023
+        </span>
+      </LineInfo>
+      <LineInfo title={'Vence em:'}>
+        <span className="font-sanofiSansRegular text-2xl leading-8">
+          25/02/2023
+        </span>
+      </LineInfo>
+      <LineInfo title={'Dias no board:'}>
+        <span className="font-sanofiSansRegular text-2xl leading-8">03</span>
+      </LineInfo>
+      <LineInfo title={'Prioridade:'}>
+        <PriorityTag priority={'Urgente'} variant={'primary'} />
+      </LineInfo>
+    </div>
+  );
+}

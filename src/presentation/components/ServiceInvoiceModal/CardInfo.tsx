144933c32802de3339c4dfa531e4c9e094ce6700
Eduardo Muchak <eduardomuchak@gmail.com>
2023-04-11 17:30:23 -0300

    feat(wip): :sparkles: add cardinfo component on timeline tab

--- src/presentation/components/ServiceInvoiceModal/CardInfo.tsx
+++ src/presentation/components/ServiceInvoiceModal/CardInfo.tsx
@@ -1,7 +1,74 @@
+import { MdComment } from 'react-icons/md';
+
+import { Input } from '../Input';
+
 export function CardInfo() {
   return (
-    <>
-      <span>Teste 3</span>
-    </>
+    <div className="flex items-center w-full pl-2">
+      {/* Círculo */}
+      {/* <div className="relative">
+        <div className="border border-black w-[30px]" />
+        <div className="absolute inset-0 m-auto mr-3 w-6 h-6 rounded-full border border-sanofiPurpleDark bg-white" />
+        <div className="absolute inset-0 m-auto mr-4 w-4 h-4 rounded-full bg-sanofiPurpleDark" />
+      </div> */}
+
+      {/* Card */}
+      <div className="bg-sanofiGray border border-black rounded-[32px] p-4 flex flex-col gap-4 w-full">
+        <span className="font-sanofiSansRegular text-base leading-5">
+          12/01/2023 - 15:25:23 até 12/01/2023 - 15:26:43
+        </span>
+        <div className="flex flex-col gap-2">
+          <span className="font-sanofiSansRegular text-base leading-5">
+            Atividade
+          </span>
+          <span className="font-sanofiSansBold text-base leading-5">
+            SAP document. check
+          </span>
+        </div>
+        <div className="flex flex-col gap-2">
+          <span className="font-sanofiSansRegular text-base leading-5">
+            Tipo de processo
+          </span>
+          <span className="font-sanofiSansBold text-base leading-5">
+            Processar fatura com OC (OC) OCR
+          </span>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 grid-flow-row gap-4 pr-0 md:pr-8">
+          <div className="flex flex-col gap-2">
+            <span className="font-sanofiSansRegular text-base leading-5">
+              Status
+            </span>
+            <span className="font-sanofiSansBold text-base leading-5">OK</span>
+          </div>
+          <div className="flex flex-col gap-2">
+            <span className="font-sanofiSansRegular text-base leading-5">
+              Status do documento
+            </span>
+            <span className="font-sanofiSansBold text-base leading-5">
+              Bloqueado
+            </span>
+          </div>
+          <div className="flex flex-col gap-2">
+            <span className="font-sanofiSansRegular text-base leading-5">
+              Função Real
+            </span>
+            <span className="font-sanofiSansBold text-base leading-5">-</span>
+          </div>
+          <div className="flex flex-col gap-2">
+            <span className="font-sanofiSansRegular text-base leading-5">
+              Agente
+            </span>
+            <span className="font-sanofiSansBold text-base leading-5">
+              Juliana Silva Ferreira
+            </span>
+          </div>
+        </div>
+        <button className="bg-white text-sanofiPurpleDark border-2 border-sanofiPurpleDark hover:bg-sanofiPurpleDark hover:text-white disabled:bg-sanofiGray disabled:text-sanofiPurpleDark flex items-center justify-center w-fit rounded-full transition-all disabled:hover:cursor-not-allowed px-4 py-2 gap-[10px] text-xl">
+          <MdComment size={24} />
+          <span>Ler Comentário</span>
+        </button>
+        <Input label="Comentário Teste" id="comentario" value={''} />
+      </div>
+    </div>
   );
 }
--- src/presentation/components/ServiceInvoiceModal/tabs/TimeLineTab.tsx
+++ src/presentation/components/ServiceInvoiceModal/tabs/TimeLineTab.tsx
@@ -1,7 +1,12 @@
+import { CardInfo } from '../CardInfo';
+
 export function TimeLineTab() {
   return (
-    <>
-      <span>Teste 1</span>
-    </>
+    <div className="flex flex-col gap-14 h-[500px] overflow-auto">
+      <CardInfo />
+      {/* <CardInfo /> */}
+      {/* <CardInfo /> */}
+      {/* <CardInfo /> */}
+    </div>
   );
 }

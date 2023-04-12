3875f5af68b6a001adfff7ca27cef1d18210bd9c
Eduardo Muchak <eduardomuchak@gmail.com>
2023-04-12 13:34:09 -0300

    fix: :art: adjusts grid layout of data container section component

--- src/presentation/components/ServiceInvoiceModal/DataContainerSection.tsx
+++ src/presentation/components/ServiceInvoiceModal/DataContainerSection.tsx
@@ -10,9 +10,9 @@ export function DataContainerSection(props: ContainerSectionProps) {
       {props.text ? (
         <div>
           <span className="font-sanofiSansBold text-[20px]">{props.title}</span>
-          <div className="text-[24px] grid grid-cols-2 grid-flow-row w-full">
-            <span>{props.number}</span>
-            <span>{props.text}</span>
+          <div className="text-[24px] grid grid-cols-5 grid-flow-row w-full">
+            <span className="col-span-2">{props.number}</span>
+            <span className="col-span-3">{props.text}</span>
           </div>
         </div>
       ) : (
